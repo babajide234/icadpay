19e4cf0848d65c205c10ce12f2560e6bb66a4720
Babajide Tomoshegbo <babajide234@gmail.com>
2022-04-24 23:13:31 +0100

    mobile menu_ testing

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,7 +1,19 @@
-import { Menu, MenuItem, NavContainer, NavLogo , ButtonContainer, Nav } from "./NavbarElements"
+import react, {useState} from "react";
+import { Menu, MenuItem, NavContainer, NavLogo , ButtonContainer, Nav, MobileMenu} from "./NavbarElements"
 import Link from "next/link"
 import Button from "../button/button"
+
+import { FiMenu } from "react-icons/fi";
+import { GrFormClose } from "react-icons/gr";
+
 export default function Navbar(){
+    const [open,setopen] =useState(false)
+    const toggleMenu = (e)=>{
+        e.preventDefault();
+
+        setopen(!open)
+        console.log('menu: ',open);
+    }
     return(
         <NavContainer>
             <Nav>
@@ -15,6 +27,25 @@ export default function Navbar(){
             <ButtonContainer>
                 <Button text={`Register`}/>
             </ButtonContainer>
+            <MobileMenu open={open}>
+                <button className="menubtn" onClick={toggleMenu}>
+                    <FiMenu/>
+                </button>
+                <div className="mobile_container">
+                    <button className="mobile_close" onClick={toggleMenu}>
+                        <GrFormClose/>
+                    </button>
+                    <div className="mobile_menu">
+                        <NavLogo src="/img/logo.png" alt=""/>
+                        <ul>
+                            <li className=""><Link href='/'>Pay Bills </Link></li>
+                            <li className=""><Link href='/'>For Businessess</Link></li>
+                            <li className=""><Link href='/'>Documentation </Link></li>
+                        </ul>
+                        <Button text={`Register`}/>
+                    </div>
+                </div>
+            </MobileMenu>
         </NavContainer>
     )
 }
--- components/navbar/NavbarElements.js
+++ components/navbar/NavbarElements.js
@@ -11,11 +11,16 @@ export const NavContainer = styled.div`
     position: fixed;
     background: #FFFFFF;
     z-index: 1000;
+    @media screen and (max-width: 480px) {
+        height: 99px;
+        padding: 0px 32px;
+    }
     /* border-bottom: 1px solid #ccc; */
 `
 export const Nav = styled.div`
     display: flex;
     align-items: center;
+    
 `
 export const NavLogo = styled.img`
     height: 39px;
@@ -25,6 +30,9 @@ export const NavLogo = styled.img`
 export const Menu = styled.ul`
     display:flex;
     justify-content: space-between;
+    @media screen and (max-width: 480px) {
+        display: none;
+    }
 `
 export const MenuItem = styled.li`
     margin-right: 49px;
@@ -45,4 +53,71 @@ export const MenuItem = styled.li`
 
 export const ButtonContainer = styled.div`
     /* width: 30%; */
+    @media screen and (max-width: 480px) {
+        display:none;
+    }
+`
+export const MobileMenu = styled.div`
+    /* width: 30%; */
+    button{
+        background: none;
+        border: none;
+        outline: none;
+    }
+    button.menubtn{
+        background: none;
+        border: none;
+        outline: none;
+        font-size: 30px;
+    }
+    .mobile{
+        &_container{
+        height: 100vh;
+        width: 80%;
+        background: #FFFFFF;
+        position: absolute;
+        left: ${props=>props.open ? '0':'-100%'};
+        top: 0;
+        box-shadow: 84px 0px 0px 0px rgba(0,0,0,0.22);
+        padding: 120px 55px;
+        overflow: hidden;
+        transition: all ease-in-out .3s;
+        }
+        &_close{
+        height: 30px;
+        width: 30px;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #E7E8E5;
+        color: #FFFFFF;
+        font-size: 30px;
+        position: absolute;
+        top: 30px;
+        right: 30px;
+        }
+        &_menu{
+            display: flex;
+            flex-flow: column;
+            align-items: center;
+            ul{
+                width: 100%;
+                padding: 0;
+                margin-top: 100px;
+                li{
+                    margin-bottom: 44px;
+                    a{
+                        font-family: Poppins;
+                        font-size: 17px;
+                        font-weight: 500;
+                        line-height: 26px;
+                        letter-spacing: 0em;
+                        text-align: left;
+
+                    }
+                }
+            }
+        }
+    }
 `
