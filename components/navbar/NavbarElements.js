829c6f900d327dde4db5e340f7746dcdd1cdc54f
Babajide Tomoshegbo <babajide234@gmail.com>
2022-04-29 07:18:57 +0100

    reinstalled packages

--- components/navbar/NavbarElements.js
+++ components/navbar/NavbarElements.js
@@ -10,7 +10,7 @@ export const NavContainer = styled.div`
     padding:0px 80px;
     position: fixed;
     background: #FFFFFF;
-    z-index: 1000;
+    z-index: 900;
     @media screen and (max-width: 480px) {
         height: 99px;
         padding: 0px 32px;
