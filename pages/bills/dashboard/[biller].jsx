0f6f59cb32b4d5ef8144de34082c856db093f013
Babajide Tomoshegbo <babajide234@gmail.com>
2022-04-29 08:34:54 +0100

    sas

--- pages/bills/dashboard/[biller].jsx
+++ pages/bills/dashboard/[biller].jsx
@@ -29,7 +29,7 @@ export async function getStaticPaths(context) {
     
     return {
         paths: [], //indicates that no page needs be created at build time
-        fallback: true //indicates the type of fallback
+        fallback: false //indicates the type of fallback
     }
 }
 
